c40a1a65fd15200529e9d1f268a95d09a7ab1c28
Sharika Khondaker <sharika.khondaker@gmail.com>
2023-04-22 19:38:34 -0400

    quicksort :)

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,92 +1,118 @@
 #include <iostream>
 #include <vector>
-
-void quickSort(std::vector<float>& numbers, float low, float high);
-float split(std::vector<float>& numbers, float low, float high);
-void swap(float *up, float *down);
+#include "recipe.h"
+#include "QuickSort.h"
 
 
-int main()
-{
-    // make a vector of floats to test with 
-    std::vector<float> numVector; 
-    numVector.push_back(3); 
-    numVector.push_back(6); 
-    numVector.push_back(2); 
-    numVector.push_back(7); 
-    numVector.push_back(9); 
-    numVector.push_back(4); 
-    numVector.push_back(1); 
-    numVector.push_back(5);
-
-    float n = numVector.size();
-    quickSort(numVector, 0, n - 1);
-
-    // print out the vector after
-    for(int i = 0; i < numVector.size(); i++)
-    {
-        std::cout << numVector.at(i) << std::endl;
-    }
-
-}
+//int main()
+//{
+//    // make a vector of floats to test with
+//    std::vector<float> numVector;
+//    numVector.push_back(3);
+//    numVector.push_back(6);
+//    numVector.push_back(2);
+//    numVector.push_back(7);
+//    numVector.push_back(9);
+//    numVector.push_back(4);
+//    numVector.push_back(1);
+//    numVector.push_back(5);
+//
+//    float n = numVector.size();
+//    quickSort(numVector, 0, n - 1);
+//
+//    // print out the vector after
+//    for(int i = 0; i < numVector.size(); i++)
+//    {
+//        std::cout << numVector.at(i) << std::endl;
+//    }
+//
+//}
 
 
 // cited from Sorting Lecture, slide 121 and 122 
-void quickSort(std::vector<float>& numbers, float low, float high)
+void quickSort(std::vector<Recipe>& numbers, float low, float high, int nutrientChoice)
 {
     // need pivot, up and down pointers
     if(low < high)
     {
         // pivot is the first element, but it gets swapped with the down pointer
-        float pivot = split(numbers, low, high);
+        float pivot = 0;//split(numbers, low, high, nutrientChoice);
         // sort the numbers below the pivot
-        quickSort(numbers, low, pivot - 1);
+        quickSort(numbers, low, pivot - 1, nutrientChoice);
         // sort the numbers above the pivot
-        quickSort(numbers, pivot + 1, high);
+        quickSort(numbers, pivot + 1, high, nutrientChoice);
     }
-
 }
 
-float split(std::vector<float>& numbers, float low, float high)
+float split(std::vector<Recipe>& numbers, float low, float high, int nutrientChoice)
 {
-    // pivot is the first element in the vector
-    float pivot = numbers[low];
-    // up is initialized to 0 index (1st element)
-    float up = low;
-    // down is initialized to last element in vector
-    float down = high;
-
-    // quicksort algorithm. when up passes down, swap pivot and down
-    while(up < down)
+    // implement the switch casing
+    if(nutrientChoice == 1)
     {
-        // move up until it finds a value > pivot
-        for(float j = up; j < high; j++)
-        {
-            if(numbers[up] > pivot)
-            {
-                break;
-            }
-            up++;
-        }
-        // move down until it finds a value <= 7
-        for(float j = high; j > low; j--)
-        {
-            if(numbers[down] < pivot)
-            {
-                break;
-            }
-            down--;
-        }
-        // swap up and down
-        if(up < down)
-        {
-            swap(&numbers[up],&numbers[down]);
-        }
+//        // pivot is the first element in the vector
+//        float pivot = numbers[low].;
+//        // up is initialized to 0 index (1st element)
+//        float up = low;
+//        // down is initialized to last element in vector
+//        float down = high;
+//
+//        // quicksort algorithm. when up passes down, swap pivot and down
+//        while(up < down)
+//        {
+//            // move up until it finds a value > pivot
+//            for(float j = up; j < high; j++)
+//            {
+//                if(numbers[up] > pivot)
+//                {
+//                    break;
+//                }
+//                up++;
+//            }
+//            // move down until it finds a value <= 7
+//            for(float j = high; j > low; j--)
+//            {
+//                if(numbers[down] < pivot)
+//                {
+//                    break;
+//                }
+//                down--;
+//            }
+//            // swap up and down
+//            if(up < down)
+//            {
+//                swap(&numbers[up],&numbers[down]);
+//            }
+//        }
+//        // swap pivot(0 index) and down
+//        swap(&numbers[low], &numbers[down]);
+//        // return down
+//        return down;
     }
-    // swap pivot(0 index) and down
-    swap(&numbers[low], &numbers[down]);
-    // return down
-    return down;
+    else if(nutrientChoice == 2)
+    {
+
+    }
+    else if(nutrientChoice == 3)
+    {
+
+    }
+    else if(nutrientChoice == 4)
+    {
+
+    }
+    else if(nutrientChoice == 5)
+    {
+
+    }
+    else if(nutrientChoice == 6)
+    {
+
+    }
+    else if(nutrientChoice == 7)
+    {
+
+    }
+
 }
 
 void swap(float *up, float *down)
---
+++ QuickSort.h
@@ -0,0 +1,4 @@
+
+void quickSort(std::vector<Recipe>& numbers, float low, float high);
+float split(std::vector<Recipe>& numbers, float low, float high);
+void swap(float *up, float *down);
